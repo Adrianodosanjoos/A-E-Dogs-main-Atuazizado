0a499303311f4872b5f9f37f927facac9b673e84
adrianodosanjoos <adriano.luizysa@gmail.com>
2024-09-25 18:07:59 -0300

    ultima atualização

--- src/database/migrations/20240922175515-create-products-table.js
+++ src/database/migrations/20240922175515-create-products-table.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
   
-      await queryInterface.createTable('Products', {
+      await queryInterface.createTable('products', {
          id: {
           type: Sequelize.INTEGER,
           allowNull: false,
