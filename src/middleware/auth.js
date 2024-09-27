05360ccb25beb6927fd79d12ebe117933458ea0c
adrianodosanjoos <adriano.luizysa@gmail.com>
2024-09-27 19:02:03 -0300

    Atualização recente

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,10 @@
 function authMiddleware(request, response, next) {
-    console.log(request.headers);
+    console.log(request.headers.authorization);
+    const authToken = request.headers.authorization;
+
+    if (authToken) {
+        return response.status(401).json({error:'Token not provided'})
+    }
 
     return next();
 }
